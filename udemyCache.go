e8338b6e8027c9b83ecb427d1750b6847bcba851
Evhenii Serbul <serbulwork@gmail.com>
2023-05-28 15:59:58 +0300

    Ready package

--- udemyCache.go
+++ udemyCache.go
@@ -5,3 +5,23 @@ import "fmt"
 func TestHello() {
 	fmt.Println("Hello! This is my first package in GitHub!")
 }
+
+type Cache struct {
+	userId map[string]interface{}
+}
+
+func New() *Cache {
+	return &Cache{make(map[string]interface{})}
+}
+
+func (c *Cache) Set(key string, value interface{}) {
+	c.userId[key] = value
+}
+
+func (c *Cache) Get(key string) interface{} {
+	return c.userId[key]
+}
+
+func (c *Cache) Delete(key string) {
+	delete(c.userId, key)
+}
